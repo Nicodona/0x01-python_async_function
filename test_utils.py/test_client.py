9e1fb60ae8fa57abd9991090ee0e46f0408229b5
njein <njeinicodona@gmail.com>
2023-01-29 20:32:07 +0100

    corrected client test file

--- test_utils.py/test_client.py
+++ test_utils.py/test_client.py
@@ -1,90 +1,84 @@
 #!/usr/bin/env python3
 """
-Test nested_map
+Test For Client Module
 """
-from unittest.mock import patch, MagicMock
-from unittest import TestCase
+import unittest
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
-import utils
+import client
 
 
-class TestAccessNestedMap(TestCase):
-    """ Class to test for nested_map"""
-    nested_map1 = {"a": 1}
-    nested_map2 = {"a": {"b": 2}}
-    nested_map3 = {"a": {"b": 2}}
-    path3 = ("a", "b")
-    path2 = ("a",)
-    path1 = ("a",)
-    nested_map4: dict = {}
-    nested_map5 = {"a": 1}
-    path4 = ("a",)
-    path5 = ("a", "b")
+class TestGithubOrgClient(unittest.TestCase):
+    """ Class for testing client module """
 
     @parameterized.expand([
-        ([nested_map1, path1], 1),
-        ([nested_map2, path2], {"b": 2}),
-        ([nested_map3, path3], 2)
+        ("google"),
+        ("abc"),
     ])
-    def test_access_nested_map(self, nmap, out):
-        """ test for access_nested_map """
-        self.assertEqual(utils.access_nested_map(*nmap), out)
+    @patch("client.get_json")
+    def test_org(self, org, mk_get_json):
+        """ Test Org property"""
+
+        gc = client.GithubOrgClient(org)
+        url = gc.ORG_URL.format(org=org)
+        gc.org()
+        mk_get_json.assert_called_once_with(url)
+
+    def test_public_repos_url(self):
+        """ test property"""
+        with patch("client.GithubOrgClient.org",
+                   new_callable=PropertyMock) as my_org:
+            my_org.return_value = {"repos_url": "www.google.com"}
+            gc = client.GithubOrgClient("google")
+            self.assertEqual(gc._public_repos_url, "www.google.com")
+
+    @patch("client.get_json")
+    def test_public_repos(self, mock_json):
+        """ Test public_repos """
+        mock_json.return_value = [{'license': {'key': 'apache-2.0',
+                                               'name': 'Apache License 2.0',
+                                               'node_id': 'MDc6TGljZW5zZTI=',
+                                               'spdx_id': 'Apache-2.0'},
+                                   'name': 'truth'
+                                   },
+                                  {
+            'license': None,
+            'name': 'ruby-openid-apps-discovery'
+        },
+            {
+                'license': {'key': 'apache-2.0',
+                            'name': 'Apache License 2.0',
+                            'node_id': 'MDc6TGljZW5zZTI=',
+                            'spdx_id': 'Apache-2.0',
+                            },
+                'name': 'autoparse'
+        },
+            {
+                                      'license': {'key': 'other',
+                                                  'name': 'Other',
+                                                  'node_id': 'MDc6TG=',
+                                                  'spdx_id': 'NOASSERTION',
+                                                  'url': None},
+
+                                      'name': 'anvil-build'}
+        ]
+        with patch("client.GithubOrgClient._public_repos_url"
+                   ) as mock_repo_url:
+            mock_repo_url.return_value = "https://api.google.com/repos"
+            gc = client.GithubOrgClient("google")
+            gc._public_repos_url()
+            self.assertEqual(gc.public_repos(), [
+                             'truth',
+                             'ruby-openid-apps-discovery',
+                             'autoparse', 'anvil-build'])
+            self.assertEqual(gc.public_repos("other"), ['anvil-build'])
+            mock_json.assert_called_once()
+            mock_repo_url.assert_called_once()
 
     @parameterized.expand([
-        (nested_map4, path4, KeyError('a')),
-        (nested_map5, path5, KeyError('b'))
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
     ])
-    def test_access_nested_map_exception(self, nmap, path, out):
-        """ test exception"""
-        with self.assertRaises(KeyError) as exc:
-            utils.access_nested_map(nmap, path)
-        self.assertIsInstance(exc.exception, KeyError)
-        self.assertEqual(str(exc.exception), str(out))
-
-
-class TestGetJson(TestCase):
-    """ Test for get json """
-
-    test_url_1 = "http://example.com"
-    test_payload_1 = {"payload": True}
-    test_url_2 = "http://holberton.io"
-    test_payload_2 = {"payload": False}
-
-    @parameterized.expand([
-        (test_url_1, test_payload_1),
-        (test_url_2, test_payload_2)
-    ])
-    def test_get_json(self, url, out):
-        """ test get json method"""
-        with patch("utils.requests.get") as resp_mock:
-            resp = MagicMock()
-            resp.json.return_value = out
-            resp_mock.return_value = resp
-            self.assertEqual(utils.get_json(url), out)
-            resp_mock.assert_called_once_with(url)
-
-
-class TestMemoize(TestCase):
-    """ test momoization"""
-
-    def test_memoize(self):
-        """ test memoize"""
-        class TestClass:
-            """ test class """
-
-            def a_method(self):
-                """ dummy method"""
-                return 42
-
-            @utils.memoize
-            def a_property(self):
-                """ dummy property"""
-                return self.a_method()
-
-        with patch.object(TestClass, "a_method") as mk:
-            mk.return_value = 42
-            tst = TestClass()
-            res2 = tst.a_property
-            res2 = tst.a_property
-            mk.assert_called_once()
-            self.assertEqual(42, res2)
+    def test_has_license(self, repo, lic, out):
+        """ test has license """
+        self.assertEqual(client.GithubOrgClient.has_license(repo, lic), out)
